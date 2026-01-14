b28060e2e18f15b8c64e6e58c01da520fcac118e
Peter Johnson <peterandersjohnson@outlook.com>
2026-01-14 15:36:50 -0600

    Vibe coding to the max!
    Investigating java issues... never written any java in my life.

---
+++ StartNoiseTool.bat
@@ -0,0 +1,7 @@
+SET "scriptPath=%~dp0"
+set JavaHome="C:\JAVA\jdk-23\bin\java"
+
+cd "%~dp0build\libs"
+
+::%JavaHome% --add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED --add-opens=jdk.unsupported/sun.misc=ALL-UNNAMED -jar BiomeTool-0.4.9-win.jar
+%JavaHome% -jar NoiseTool-1.2.1-all.jar
